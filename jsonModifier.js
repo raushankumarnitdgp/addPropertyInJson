8f6981aafc1c4e7247992f653746c0a9245c07d6
raushankumarnitdgp <raushankumar.nitdgp@gmail.com>
2017-10-27 14:49:33 +0530

    Added test files

--- jsonModifier.js
+++ jsonModifier.js
@@ -1,5 +1,14 @@
 var fs = require('fs');
 var path = require('path');
 
-var publicModules  = require(path.join(__dirname,'./input/dependency.json'));
+var publicModules  = require(path.join(__dirname,'./input/xt-edge-storage.json'));
+// console.log(publicModules);
+var category = ['chart',]
+
+for(var plotObj in publicModules) {
+    publicModules[plotObj].description = 'Visualize Data in ' + plotObj;
+    publicModules[plotObj].displayName = plotObj;
+    publicModules[plotObj].category = {};
+}
+
 console.log(publicModules);
